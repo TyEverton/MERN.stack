3e1b59c5c43637d21179f062d5295371278fe588
Ty Everton <everton_915@yahoo.com>
2022-03-28 18:25:44 -0600

    Completed crud backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+
 // @desc = description.
 // @ desc Get Goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get Goals!' })
+  const goals = await Goal.find()
+  res.status(200).json(goals)
 })
 
 // @desc = description.
@@ -16,7 +19,10 @@ const setGoals = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please fill in the text field with your desired goal.')
   }
-  res.status(200).json({ message: 'Set your goal.' })
+  const goal = await Goal.create({
+    text: req.body.text,
+  })
+  res.status(200).json(goal)
 })
 
 // @desc = description.
@@ -24,15 +30,34 @@ const setGoals = asyncHandler(async (req, res) => {
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+  res.status(200).json(updatedGoal)
 })
 
 // @desc = description.
 // @ desc Get Goals
 // @route DELETE /api/goals/:id
 // @access Private
+//the findByIdAndRemove is a mongoDB function
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Deleted goal ${req.params.id}` })
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  //   const removedGoal = await Goal.findByIdAndRemove(req.params.id, req.body) | this can also be done with the code below
+  await goal.remove()
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 // const port line below says to run the server on the .env port number, or port 5000 if the .env port is unavailable
 const port = process.env.port || 5000
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
